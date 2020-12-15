b43127d13e106988ddabd54688ed51ca8f689015
Stephanie Wahl <aunt.tepe@gmail.com>
2020-12-14 22:25:27 -0500

    starting updateRemaining in js

--- scripts.js
+++ scripts.js
@@ -19,33 +19,44 @@ function drawChart() {
   chart.draw(data, options);
 }
 
-// document
-//     .querySelector("#update_income")
-//     .addEventListener("click", updateBudget);
+document
+    .querySelector("#submit_budget_amount")
+    .addEventListener("click", updateRemaining);
     
 
 document.querySelector("#submit_expense").addEventListener("click",addExpense);
 
 let weeklyBudget = document.querySelector("#weekly_budget");
-let incomeInput = document.querySelector("#income_input");
+let entertainmentInput = document.querySelector("#ent_input");
+let entRemainingBalance = document.querySelector("#remaining_balance");
+
+let foodInput = document.querySelector("#food_input");
+let clothingInput = document.querySelector("#clothing_input");
+let billsInput = document.querySelector("#bills_input");
+
 let remainingBalance = document.querySelector("#remaining_balance");
 let amountInput = document.querySelector("#amount_input");
 let nameInput = document.querySelector("#name_input");
 let expenseList = document.querySelector("#expense_list");
 let totalExpenses = document.querySelector("#total_expenses");
 
-let weeklyIncome = 0;
+let totalBudget = 0;
 let expenses = [];
 let expenseTotal = 0;
 let balance = 0;
 
-// function updateBudget(event) {
-//     event.preventDefault();
-//     monthlyIncome = incomeInput.value;
-//     monthlyBudget.innerText = "$" + monthlyIncome;
-//     incomeInput.value = "";
-//     updateBalance();    
-// }
+function updateRemaining(event) {
+    event.preventDefault();
+    entertainmentInput = entertainmentInput.value;
+    let entRemainingBalance = document.getElementById("ent_remaining");
+    let remaining = document.createElement("p");
+    entRemainingBalance.appendChild("remaining");
+
+
+    // console.log(entertainmentInput);
+    
+   
+}
 
 // function updateBalance() {
 //     balance = monthlyIncome - expenseTotal;
