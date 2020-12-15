4b1a664e4120453f0f2d8325fc9e7aec6b65b923
Stephanie Wahl <aunt.tepe@gmail.com>
2020-12-15 13:16:45 -0500

    js work

--- scripts.js
+++ scripts.js
@@ -19,16 +19,12 @@ function drawChart() {
   chart.draw(data, options);
 }
 
-document
-    .querySelector("#submit_budget_amount")
-    .addEventListener("click", updateRemaining);
-    
-
+document.querySelector("#submit_budget_amount").addEventListener("click", updateRemaining);
 document.querySelector("#submit_expense").addEventListener("click",addExpense);
 
-let weeklyBudget = document.querySelector("#weekly_budget");
+// let weeklyBudget = document.querySelector("#weekly_budget");
 let entertainmentInput = document.querySelector("#ent_input");
-let entRemainingBalance = document.querySelector("#remaining_balance");
+let entRemainingBalance = document.querySelector("#ent_remaining");
 
 let foodInput = document.querySelector("#food_input");
 let clothingInput = document.querySelector("#clothing_input");
@@ -45,30 +41,40 @@ let expenses = [];
 let expenseTotal = 0;
 let balance = 0;
 
-function updateRemaining(event) {
-    event.preventDefault();
-    entertainmentInput = entertainmentInput.value;
-    let entRemainingBalance = document.getElementById("ent_remaining");
-    let remaining = document.createElement("p");
-    entRemainingBalance.appendChild("remaining");
+// function updateRemaining(event) {
+//     event.preventDefault();
+//     entertainmentInput = entertainmentInput.value;
+//     // let entRemainingBalance = document.getElementById("ent_remaining");
+//     // let remaining = document.createElement("p");
+//     entertainmentInput.append(entRemainingBalance);
 
 
-    // console.log(entertainmentInput);
-    
-   
+//     console.log(entertainmentInput);
+//     console.log(entRemainingBalance);
+  
+// }
+let entertainmentInput = document.querySelector("#ent_input");
+let entRemainingBalance = document.querySelector("#ent_remaining");
+
+function updateBudget(event) {
+  event.preventDefault();
+  monthlyIncome = incomeInput.value;
+  monthlyBudget.innerText = "$" + monthlyIncome;
+  incomeInput.value = "";
+  updateBalance();    
 }
 
-// function updateBalance() {
-//     balance = monthlyIncome - expenseTotal;
-//     remainingBalance.innerText = "$" + balance;
-//     if (balance < 0) {
-//         remainingBalance.classList.remove("green");
-//         remainingBalance.classList.add("red");
-//     }   else {
-//         remainingBalance.classList.remove("red");
-//         remainingBalance.classList.add("green");
-//     }
-// }
+function updateBalance() {
+    balance = monthlyIncome - expenseTotal;
+    remainingBalance.innerText = "$" + balance;
+    if (balance < 0) {
+        remainingBalance.classList.remove("green");
+        remainingBalance.classList.add("red");
+    }   else {
+        remainingBalance.classList.remove("red");
+        remainingBalance.classList.add("green");
+    }
+}
 
 function addExpense(event) {
     event.preventDefault();
